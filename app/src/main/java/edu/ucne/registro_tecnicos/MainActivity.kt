5e1d85692c56519df056143797e3ad426898989e
BrynOvalles <brynovallesburgos@gmail.com>
2025-01-12 21:30:11 -0400

    Creacion de la Apliacion sin Utilizar carpetas

--- app/src/main/java/edu/ucne/registro_tecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registro_tecnicos/MainActivity.kt
@@ -4,44 +4,285 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.Upsert
 import edu.ucne.registro_tecnicos.ui.theme.Registro_TecnicosTheme
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
+import java.text.NumberFormat
+
 
 class MainActivity : ComponentActivity() {
+    private lateinit var tecnicoDb: TecnicoDb
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        tecnicoDb = Room.databaseBuilder(
+            applicationContext,
+            TecnicoDb::class.java,
+            "Tecnicos.db"
+        ).fallbackToDestructiveMigration()
+            .build()
+
         setContent {
             Registro_TecnicosTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        TecnicosScreen()
+                    }
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+    @Composable
+    fun TecnicosScreen(
+
+    ) {
+        var nombre by remember { mutableStateOf("") }
+        var sueldo by remember { mutableStateOf("") }
+        var errormessaage: String? by remember { mutableStateOf(null) }
+
+        Scaffold { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(16.dp)
+            ) {
+                ElevatedCard(
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    ) {
+                        OutlinedTextField(
+                            label = { Text("Nombre") },
+                            value = nombre,
+                            onValueChange = { nombre = it },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        OutlinedTextField(
+                            label = { Text("Sueldo") },
+                            value = sueldo,
+                            onValueChange = { sueldo = it },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        Spacer(modifier = Modifier.padding(2.dp))
+                        errormessaage?.let {
+                            Text(text = it, color = Color.Red)
+                        }
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                        ) {
+                            OutlinedButton(
+                                onClick = {
+
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Add,
+                                    contentDescription = "new button"
+                                )
+                                Text("Nuevo")
+                            }
+                            val scope = rememberCoroutineScope()
+                            OutlinedButton(
+                                onClick = {
+                                    if (nombre.isBlank() || sueldo.isBlank()) {
+                                        errormessaage =
+                                            "El nombre o el sueldo no pueden estar vacio"
+                                    } else {scope.launch {
+                                        saveTecnico(
+                                            TecnicoEntity(
+                                                nombre = nombre,
+                                                sueldo = sueldo.toInt()
+                                            )
+                                        )
+                                        nombre = ""
+                                        sueldo = ""
+                                        errormessaage = ""
+                                        }
+                                    }
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Check,
+                                    contentDescription = "save button"
+                                )
+                                Text(text = "Guardar")
+                            }
+                        }
+                    }
+                }
+
+                val lifecycleOwner = androidx.lifecycle.compose.LocalLifecycleOwner.current
+                val tecnicosList by tecnicoDb.tecnicoDao().getAll()
+                    .collectAsStateWithLifecycle(
+                        initialValue = emptyList(),
+                        lifecycleOwner = lifecycleOwner,
+                        minActiveState = Lifecycle.State.STARTED
+                    )
+                TecnicosListScreen(tecnicosList)
+
+            }
+        }
+    }
+
+    @Composable
+    fun TecnicosListScreen(tecnicosList: List<TecnicoEntity>) {
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Spacer(modifier = Modifier.height(42.dp))
+            Text("Listado de Tecnicos")
+            HorizontalDivider()
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 2.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(text = "Id", modifier = Modifier.weight(1f), color = Color.Cyan)
+                Text(text = "Nombre", modifier = Modifier.weight(1f), color = Color.Cyan)
+                Text(text = "Sueldo", modifier = Modifier.weight(1f), color = Color.Cyan)
+            }
+
+            HorizontalDivider()
+
+            LazyColumn(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                items(tecnicosList) {
+                    TecnicosRow(it)
+                }
+            }
+        }
+    }
+
+    @Composable
+    private fun TecnicosRow(it: TecnicoEntity) {
+        val formato = "RD ${NumberFormat.getNumberInstance().format(it.sueldo)}"
+        Row (
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(modifier = Modifier.weight(1f), text = it.tecnicosId.toString())
+            Text(
+                modifier = Modifier.weight(1f),
+                text = it.nombre
+            )
+            Text(modifier = Modifier.weight(1f), text = formato)
+        }
+        HorizontalDivider()
+    }
+
+    private suspend fun saveTecnico(tecnico: TecnicoEntity) {
+        tecnicoDb.tecnicoDao().save(tecnico)
+    }
+
+    @Entity(tableName = "Tecnicos")
+    data class TecnicoEntity(
+        @PrimaryKey
+        val tecnicosId: Int? = null,
+        val nombre: String = "",
+        val sueldo: Int = 0
     )
-}
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Registro_TecnicosTheme {
-        Greeting("Android")
+    @Dao
+    interface TecnicoDao {
+        @Upsert()
+        suspend fun save(tecnico: TecnicoEntity)
+
+        @Query(
+            """
+            SELECT *
+            FROM Tecnicos
+            WHERE tecnicosId=:id
+            LIMIT 1
+        """
+        )
+        suspend fun find(id: Int): TecnicoEntity?
+
+        @Delete
+        suspend fun delete(tecnico: TecnicoEntity)
+
+        @Query("SELECT * FROM Tecnicos")
+        fun getAll(): Flow<List<TecnicoEntity>>
+    }
+
+    @Database(
+        entities = [TecnicoEntity::class],
+        version = 1,
+        exportSchema = false
+    )
+    abstract class TecnicoDb : RoomDatabase() {
+        abstract fun tecnicoDao(): TecnicoDao
+    }
+
+    @Preview(showBackground = true, showSystemUi = true)
+    @Composable
+    fun Preview() {
+        Registro_TecnicosTheme {
+            val tecnicosList = listOf(
+                TecnicoEntity(
+                    tecnicosId = 1,
+                    nombre = "Enel",
+                    sueldo = 10000
+                ),
+                TecnicoEntity(
+                    tecnicosId = 2,
+                    nombre = "Bryan",
+                    sueldo = 12500),
+            )
+            TecnicosListScreen(tecnicosList)
+        }
     }
 }
