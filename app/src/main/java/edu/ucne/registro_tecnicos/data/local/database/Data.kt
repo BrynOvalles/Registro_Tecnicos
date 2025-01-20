a23464991e556fb93b1ad8b5976eace448866f6b
BrynOvalles <brynovallesburgos@gmail.com>
2025-01-19 20:36:07 -0400

    Se Creo la Data para el acceso a la base de datos y se Actualizo el TecnicoDb para usar la nueva Data

---
+++ app/src/main/java/edu/ucne/registro_tecnicos/data/local/database/Data.kt
@@ -0,0 +1,21 @@
+package edu.ucne.registro_tecnicos.data.local.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import edu.ucne.registro_tecnicos.data.local.dao.TecnicoDao
+import edu.ucne.registro_tecnicos.data.local.dao.TicketDao
+import edu.ucne.registro_tecnicos.data.local.entity.TecnicoEntity
+import edu.ucne.registro_tecnicos.data.local.entity.TicketEntity
+
+@Database(
+        entities = [TecnicoEntity::class, TicketEntity::class],
+        version = 3,
+        exportSchema = false
+    )
+@TypeConverters(Converters::class)
+    abstract class Data : RoomDatabase() {
+        abstract fun tecnicoDao(): TecnicoDao
+        abstract fun ticketDao(): TicketDao
+    }
+
