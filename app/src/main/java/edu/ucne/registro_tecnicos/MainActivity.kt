46b00bf8fe7712a212e277d73daea9e229dcf647
BrynOvalles <brynovallesburgos@gmail.com>
2025-01-19 20:39:47 -0400

    Se implementó la base de datos Room y se agregaron los repositorios, modificando la estructura de la clase MainActivity.

--- app/src/main/java/edu/ucne/registro_tecnicos/MainActivity.kt
+++ app/src/main/java/edu/ucne/registro_tecnicos/MainActivity.kt
@@ -11,25 +11,28 @@ import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import edu.ucne.registro_tecnicos.Presentation.Navigation.TecnicosNavHost
-import edu.ucne.registro_tecnicos.data.local.database.TecnicoDb
+import edu.ucne.registro_tecnicos.data.local.database.Data
 import edu.ucne.registro_tecnicos.data.repository.TecnicoRepository
+import edu.ucne.registro_tecnicos.data.repository.TicketRepository
 import edu.ucne.registro_tecnicos.ui.theme.Registro_TecnicosTheme
 
 
 class MainActivity : ComponentActivity() {
     private lateinit var tecnicoRepository: TecnicoRepository
+    private lateinit var ticketsRepository: TicketRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val tecnicoDb = Room.databaseBuilder(
+        val dataDb = Room.databaseBuilder(
             applicationContext,
-            TecnicoDb::class.java,
+            Data::class.java,
             "Tecnicos.db"
         ).fallbackToDestructiveMigration()
             .build()
-        tecnicoRepository = TecnicoRepository(tecnicoDb)
+        tecnicoRepository = TecnicoRepository(dataDb)
+        ticketsRepository = TicketRepository(dataDb)
 
         setContent {
             Registro_TecnicosTheme {
@@ -37,13 +40,9 @@ class MainActivity : ComponentActivity() {
                     text = "Tecnicos",
                     modifier = Modifier.padding(16.dp)
                 )
-                TecnicosNavHost(tecnicoRepository, rememberNavController()
+                TecnicosNavHost(tecnicoRepository, ticketsRepository, rememberNavController()
                 )
             }
         }
     }
-
-//    private suspend fun saveTecnico(tecnico: TecnicoEntity) {
-//        tecnicoDb.tecnicoDao().save(tecnico)
-//    }
 }
